0db71060afe784d8808544dd1626181aa3ece688
Astrid Hanghøj <astridhanghoej@gmail.com>
2023-10-17 10:35:14 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = -40
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles14, function (sprite, location) {
+    mySprite.vy = -80
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f 4 4 f . . . 
+    . . . . f f b f 5 4 5 5 4 f . . 
+    . . . f b 3 3 e 4 5 5 5 5 f . . 
+    . . f b 3 3 3 3 e 4 4 4 e f . . 
+    . . f 3 3 3 3 3 3 3 3 3 3 3 f . 
+    . . f 3 3 3 3 e b 3 e e 3 3 f . 
+    . . f 3 3 3 3 f f e e e 3 3 f . 
+    . f f b b b b f b f e e e f . . 
+    . f b b b b b e 1 f 4 4 e . . . 
+    . f b b b b b f 4 4 4 4 f . . . 
+    . . f b b b 4 4 e d d d f . . . 
+    . . . f f f 4 4 e d d d f . . . 
+    . . . f b b e e b b d d d f . . 
+    . . . . f b d d 1 d 1 d b f . . 
+    . . . . . f f f b b f f f . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 0)
+scene.setBackgroundColor(3)
+mySprite.ay = 20
+tiles.setCurrentTilemap(tilemap`level1`)
